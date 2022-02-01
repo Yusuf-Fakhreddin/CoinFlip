6f7afc02caaf3c8a9ac125f2782e0c2d6336519b
Yusuf Fakhreddin <48655310+Yusuf-Fakhreddin@users.noreply.github.com>
2022-02-01 06:38:09 +0200

    Initial commit

---
+++ Coin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoinFlip
+{
+    internal class Coin
+    {
+        public bool isHeads { get; set; }
+
+        // constructor for new coin creation
+        public Coin(bool h)
+        {
+           this.isHeads = h;
+        }
+
+        // asign the coin heads or tails randomly 
+        public void flipCoin(Random rnd)
+        {
+            if (rnd.Next(10) > 5)
+            {
+                this.isHeads=true;
+            }
+            else
+            {
+                this.isHeads=false;
+            }
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CoinFlip
+{
+    public partial class Form1 : Form
+    {
+
+        // Array of coins
+        Coin[] coins = new Coin[5];
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        // the function that starts when the form is displayed for the first time
+        // initiation point 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "";
+
+            for (int i = 0; i < coins.Length; i++)
+            {
+                coins[i] = new Coin(false);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // change label into 5 heads or tails
+            label1.Text = "";
+            Random rnd = new Random();
+            for (int i = 0; i < coins.Length; i++)
+            {
+                label1.Text += coins[i].isHeads + ", ";
+                coins[i].flipCoin(rnd);
+            }
+            // set the pictures according to isHeads field
+            pictureBox1.Image= coins[0].isHeads? Properties.Resources.quarterHeads1 : Properties.Resources.quarterTails;
+            pictureBox2.Image = coins[1].isHeads ? Properties.Resources.quarterHeads1 : Properties.Resources.quarterTails;
+            pictureBox3.Image = coins[2].isHeads ? Properties.Resources.quarterHeads1 : Properties.Resources.quarterTails;
+            pictureBox4.Image = coins[3].isHeads ? Properties.Resources.quarterHeads1 : Properties.Resources.quarterTails;
+            pictureBox5.Image = coins[4].isHeads ? Properties.Resources.quarterHeads1 : Properties.Resources.quarterTails;
+
+        }
+    }
+}
